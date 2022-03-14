44c64cb3a06aacdb402871a277ae48542161e9dc
Huseyn <70753221+Ri7vaI@users.noreply.github.com>
2022-03-14 10:35:39 +0300

    Новые практики

---
+++ src/Component/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <header className='header'>
+            <img src='https://www.kindpng.com/picc/m/128-1280187_github-logo-png-github-transparent-png.png'/>
+        </header>
+    );
+}
+export default Header;
